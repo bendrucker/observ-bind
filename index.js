e6d88f4a4cc339c520b181e2f3315608bbba64b9
Ben Drucker <bvdrucker@gmail.com>
2015-08-31 10:27:24 -0700

    Bind o.set for use with observ-array

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 module.exports = function observBind (observ1, observ2) {
   var transaction = false
-  var unlisten1 = observ1(update(observ2.set))
-  var unlisten2 = observ2(update(observ1.set))
+  var unlisten1 = observ1(update(observ2.set.bind(observ2)))
+  var unlisten2 = observ2(update(observ1.set.bind(observ1)))
 
   return unlisten
 
